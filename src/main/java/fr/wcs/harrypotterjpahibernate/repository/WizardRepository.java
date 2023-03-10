cdac44722322af4f2ab129145f57a44b1562c0ff
kinotonik <102355464+kinotonik@users.noreply.github.com>
2023-03-10 22:53:47 +0100

    Update WizardRepository.java

--- src/main/java/fr/wcs/harrypotterjpahibernate/repository/WizardRepository.java
+++ src/main/java/fr/wcs/harrypotterjpahibernate/repository/WizardRepository.java
@@ -2,6 +2,8 @@ package fr.wcs.harrypotterjpahibernate.repository;
 
 import fr.wcs.harrypotterjpahibernate.entity.Wizard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WizardRepository extends JpaRepository<Wizard, Long> {
 }
