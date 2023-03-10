ee8c1cc35344a6160ff475b3bc049cd1b3c9fdc5
kinotonik <102355464+kinotonik@users.noreply.github.com>
2023-03-10 22:53:06 +0100

    Update SchoolRepository.java

--- src/main/java/fr/wcs/harrypotterjpahibernate/repository/SchoolRepository.java
+++ src/main/java/fr/wcs/harrypotterjpahibernate/repository/SchoolRepository.java
@@ -3,5 +3,6 @@ package fr.wcs.harrypotterjpahibernate.repository;
 import fr.wcs.harrypotterjpahibernate.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface SchoolRepository extends JpaRepository<School,Long> {
 }
